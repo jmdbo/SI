d28c5c48c16d6884cdac4cbe081ff174cf998c69
joaoaires <jo.aires@outlook.com>
2014-10-04 19:30:55 +0100

    putget update
    beta untested version of PutGet.java

--- trab1/src/trab1/PutGet.java
+++ trab1/src/trab1/PutGet.java
@@ -17,6 +17,55 @@ public class PutGet extends Action {
     @Override
     protected void doAction() throws InterruptedException {
 
+        int act_y=buffer.gety();
 
+        //TAKE FROM CELL
+        if(isGetting)
+        {
+            //GO TO ZZ BELLOW
+            while (!(buffer.getPut()==0))
+            {
+                buffer.moveXZ(0,-1);
+            }
+            //Put part into Warehouse
+            while (!(buffer.gety()==2))
+            {
+                buffer.moveY(1);
+            }
+            //GO TO ZZ ABOVE
+            while (!(buffer.getPut()==1))
+            {
+                buffer.moveXZ(0,1);
+            }
+            //Get part from Warehouse
+            while (!(buffer.gety()==1))
+            {
+                buffer.moveY(-1);
+            }
+        }
+
+        //PUT IN CELL
+        else
+        {
+            //GO TO ZZ ABOVE
+            while (!(buffer.getPut()==1))
+            {
+                buffer.moveXZ(0,1);
+            }//Put part into Warehouse
+            while (!(buffer.gety()==2))
+            {
+                buffer.moveY(1);
+            }
+            //GO TO ZZ BELLOW
+            while (!(buffer.getPut()==0))
+            {
+                buffer.moveXZ(0,-1);
+            }
+            //Get part from Warehouse
+            while (!(buffer.gety()==1))
+            {
+                buffer.moveY(-1);
+            }
+        }
     }
 }
