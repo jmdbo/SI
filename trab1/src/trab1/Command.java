da565f25cef486251e94f23e3ada0af77e79c746
joaoaires <jo.aires@outlook.com>
2014-10-07 15:04:33 +0100

    command update

--- trab1/src/trab1/Command.java
+++ trab1/src/trab1/Command.java
@@ -10,13 +10,22 @@ public class Command {
     public int z;
     public int tray;
     public boolean isGetting;
+    public boolean isTray;
 
+    //constructor GotoXZ
     public Command(String _order, int _x, int _z, int _tray, boolean _isGetting){
         this.isGetting = _isGetting;
         this.order = _order;
         this.x = _x;
         this.z = _z;
         this.tray = _tray;
+    }
+
+    //constructor PutGet
+    public Command(String _order, boolean _tray, boolean _isGetting){
+        this.isGetting = _isGetting;
+        this.order = _order;
+        this.isTray = _tray;
     }
 
     public Command(String _order){
