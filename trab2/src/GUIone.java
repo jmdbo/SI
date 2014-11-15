c578c522170564330322e16c8273311d522a1a50
Joao Aires <jo.aires@outlook.com>
2014-11-15 23:56:22 +0000

    gui

---
+++ trab2/src/GUIone.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by Aires on 15-11-2014.
+ */
+public class GUIone {
+    private JPanel panel1;
+    private JTextArea positionX09TextArea;
+    private JTextArea positionZ04TextArea;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
